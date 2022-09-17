6a0e7cc73a96316072fde753086212d20a64cb90
marius-perera <marius.perera@xero.com>
2022-09-17 23:00:51 +1000

    Add necessary packages, images and fonts

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
 import './App.css'
+
+import dttLogo from './assets/dtt_logo_transparent.png'
+import reactLogo from './assets/react.svg'
+import { useState } from 'react'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -8,14 +10,14 @@ function App() {
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+        <a href="https://www.dreamteamtravels.com.au/" target="_blank">
+          <img src={dttLogo} className="logo" alt="Dream Team Travels logo" />
         </a>
         <a href="https://reactjs.org" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>{`Vite + React ${import.meta.env.VITE_PORT}`}</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
