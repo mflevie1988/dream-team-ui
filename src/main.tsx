3bedef675a5c062b9552d3c34c68aa25580ef4d0
marius-perera <marius.perera@xero.com>
2023-04-03 23:17:46 +1000

    Resolve package dependency issues by downgrading some packages

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,18 @@
+import * as ReactDOM from 'react-dom';
+
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import React from 'react';
-import ReactDOM from 'react-dom/client';
 import store from './store';
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root') as HTMLElement
 );
